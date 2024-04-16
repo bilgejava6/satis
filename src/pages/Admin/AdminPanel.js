043b381de7dc7bce1e5ab908a86c102eb35d38fb
muhammetalikaya <muhammedali55@gmail.com>
2024-04-16 14:34:29 +0300

    öğledensonra 1.ders sonu

--- src/pages/Admin/AdminPanel.js
+++ src/pages/Admin/AdminPanel.js
@@ -1,9 +1,19 @@
 import { useSelector } from "react-redux";
 import UserProfile from "../../components/UserProfile";
+import { useState } from "react";
 
 function AdminPanel(){
-    const userProfileData = useSelector(state=> state.personel.data);
+    const userProfileData = useSelector(state=> state.personel.data);    
+    const [urun,setUrun] = useState({
+        ad: '',
+        aciklama: '',
+        fiyat: 0.0,
+        resim:''
+    });
+    const urunEkle = ()=>{
 
+    };
+    
     return(
         <div className="container">
             <div className="row mt-5 p-3 border border-primary">
@@ -16,7 +26,85 @@ function AdminPanel(){
                 </div>                    
             </div>
             <div className="row mt-1 p-3 border border-success">
-                
+                <div className="col">
+                        <div className="row">
+                            <div className="mb-3">
+                                <label className="form-label" style={{display:'block'}}>Ürün Adı</label>
+                                <input type="text" className="form-control" placeholder="ürün adı" />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label" style={{display:'block'}}>fiyat</label>
+                                <input type="text" className="form-control" placeholder="00.00₺" />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label" style={{display:'block'}}>Açıklama</label>
+                                <input type="text" className="form-control" placeholder="ürün açıklaması" />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label" style={{display:'block'}}>Resim URL</label>
+                                <input type="text" className="form-control" placeholder="http://domain.com/urun.png" />
+                            </div>
+                            <div className="mb-3">
+                                <button type="button" className="btn btn-success" style={{display:'block', width: '100%'}}>Ürün Ekle</button>
+                            </div>
+                            
+                        </div>
+
+                        <div className="row">
+                            <table className="table table-hover">
+                                <thead className="table-success">
+                                    <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Ürün Adı</th>
+                                    <th scope="col">Fiyat</th>
+                                    <th scope="col">Açıklama</th>
+                                    <th scope="col">Resim</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Ekran Kartı Asus</td>
+                                        <td>25.000 ₺</td>
+                                        <td>RTX4090</td>
+                                        <td>
+                                            <img src="https://picsum.photos/50/50" alt="" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Ekran Kartı Asus</td>
+                                        <td>25.000 ₺</td>
+                                        <td>RTX4090</td>
+                                        <td>
+                                            <img src="https://picsum.photos/50/50" alt="" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Ekran Kartı Asus</td>
+                                        <td>25.000 ₺</td>
+                                        <td>RTX4090</td>
+                                        <td>
+                                            <img src="https://picsum.photos/50/50" alt="" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Ekran Kartı Asus</td>
+                                        <td>25.000 ₺</td>
+                                        <td>RTX4090</td>
+                                        <td>
+                                            <img src="https://picsum.photos/50/50" alt="" />
+                                        </td>
+                                    </tr>
+                                    
+                                </tbody>
+                            </table>
+                        </div>
+
+
+                </div>
             </div>
         </div>
     )
--- src/store/features/urunSlice.js
+++ src/store/features/urunSlice.js
@@ -1,18 +1,24 @@
 import urunUrl from "../../config/UrunController";
-
-const { createAsyncThunk, createSlice } = require("@reduxjs/toolkit")
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 
 const initUrunState={
-    urunList: []
-}
+    urunList: [],
+    isLoadingAdd: false,
+    isLoadingGetAll: false
+};
 
 export const fetchUrunEkleme = createAsyncThunk(
     'urun/fetchUrunEkleme',
     async (payload)=>{
         try {
-         const result =  fetch(urunUrl.urunEkle,{
-            
-         });
+         const result = await fetch(urunUrl.urunEkle,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload) // {ad,aciklama,fiyat,resim}
+         }).then(data=> data.json()) // string 
+         .then(data=>data);
 
          return result;
         } catch (error) {
@@ -20,14 +26,43 @@ export const fetchUrunEkleme = createAsyncThunk(
         }
     }
 );
-export const fetchUrunListele = createAsyncThunk('urun/fetchUrunListele',{});
+export const fetchUrunListele = createAsyncThunk(
+    'urun/fetchUrunListele',
+    async ()=>{
+       const result = await fetch(urunUrl.urunListele,{
+            method:'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(data=>data.json())
+        .then(data=>data);
+        return result;
+    }
+);
 
 const urunSlice = createSlice({
     name: 'urun',
     initialState: initUrunState,
     reducers: {},
     extraReducers: (build)=>{
-
+        build.addCase(fetchUrunEkleme.pending,(state)=>{state.isLoadingAdd=true;});
+        build.addCase(fetchUrunEkleme.fulfilled,(state,action)=>{
+            state.isLoadingAdd=false;
+            if(action.payload.data){
+                alert("Ürün Başarı ile eklendi.");
+            }
+        });
+        build.addCase(fetchUrunEkleme.rejected,(state)=>{state.isLoadingAdd=false;});
+         
+        build.addCase(fetchUrunListele.pending,(state)=>{state.isLoadingGetAll=true;});
+        build.addCase(fetchUrunListele.fulfilled,(state,action)=>{
+            state.isLoadingGetAll=false;
+            if(action.payload.status===200){
+                state.urunList = action.payload.data;
+            }
+        });
+        build.addCase(fetchUrunListele.rejected,(state)=>{state.isLoadingGetAll=false;});
+        
     }
 });
 
